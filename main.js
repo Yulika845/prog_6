a841853d4f1dd219164113c247710b0c8e7e58bf
Yulika845 <63868064+Yulika845@users.noreply.github.com>
2022-06-08 23:12:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 6,
+        arrows: false,
+        dots: true
+    })
+});
